4a8637412d0c61ae6f7c1b5e0644a8b7621e3d4c
Babaralihyd <162362468+Babaralihyd@users.noreply.github.com>
2024-03-09 18:00:13 -1200

    data types file
    A- Primitive data types ,, string ,number , boolean  ,undefined ,null ,symbol  // concatenation   //annotation   camel case //   snake case   // pascal case  // update  // declaration  //redeclare  // block scope

---
+++ main.ts
@@ -0,0 +1,67 @@
+// string data types 
+console.log ('hellow world ') , 
+console.log ("Babar ali" ) ,
+console.log (`student it program`) ,
+
+// number  data types 
+console.log (300) ,
+console.log (301) ,
+console.log (302) ,
+
+// types of  data types 
+console.log (typeof 'abc ') ;
+console.log (typeof 123) ;
+ 
+//boolean data types 
+// console.log(true)
+// console.log(false)
+console.log (3===3)   ;      
+
+// concatention data types 
+console.log ('Babar', 'Ali' , 'students') ;
+console. log ('Governor'+'sindh'+'itprogram') ;           
+
+// variables data  types 
+// variable decularation let ,const .var 
+let  box = 'mobile' ;
+const container = 'water ';
+var  myvarible =  100 ; 
+
+console.log (typeof  myvarible) ;
+console .log (box) ;                                                                                                                                                                                
+
+// variable of annoleation //
+let bus : string ='green';
+const containerofwater :string= 'water';
+var  myvarible : number =  123 ; 
+    
+// variable  naming convention // 
+let governorSindhItProgram = 'students' ; // camel case 
+let governor_SindhIt_Program = 'students'; // snake case 
+let GovernorSindhItProgram = 'students' ;  // pascal case 
+
+// updation 
+let mybox = 'mobile ';  // declaration 
+mybox = 'laptop ' ;
+mybox = ' iphonemobile '; // updation 
+console.log (mybox) ;
+
+
+let sendbox = 'apple ' ;  // declaration 
+sendbox = 'mango ' ;// updation     // let is not use to again   redeclare 
+
+
+  // block scope 
+let x = 10; 
+if (x===10){  
+     var mystudent = 'beatiful';
+     let  mylaptop = 'dell ';
+     const mobile = ' qmobile ' ;
+     console.log (mystudent) ;
+
+
+
+
+
+}
+
